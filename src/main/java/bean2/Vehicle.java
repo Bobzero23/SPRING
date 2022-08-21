a653fdab1d406faa872bb28b01e61dbaacce4421
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-21 07:31:46 +0300

    MORNING SESSION PART TWO

---
+++ src/main/java/bean2/Vehicle.java
@@ -0,0 +1,21 @@
+package bean2;
+
+/*This is just a normal class to create some fields*/
+
+/*class*/
+public class Vehicle {
+
+    /*creating a field called name*/
+    private String name;
+
+    /*getter*/
+    public String getName(){
+        return name;
+    }
+
+    /*setter*/
+    public void setName(String nam){
+        this.name = name;
+    }
+
+}
