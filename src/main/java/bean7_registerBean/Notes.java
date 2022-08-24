4055f8c252e2520165a7bed1bceba279f32f2f3a
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-24 22:43:44 +0300

    CREATING BEANS PROGRAMMATICALLY

--- src/main/java/bean7_registerBean/Notes.java
+++ src/main/java/bean7_registerBean/Notes.java
@@ -1,5 +1,6 @@
 /*Sometimes we want to create bean according to a condition and by that register bean method will be helpful
 * This method accepts three parameters ("nameOfTheBean",TypeOfTheBean,ObjectOfTheSupplierInterface)
 * As you can see we use interface and that is a functional interface tho we gonna use lambda expression
+* In our code the main point is to register a bean according to random number generated
 *
 * Note: This time we don't use the component annotation in the field classes */
