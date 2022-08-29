d672e1d647eed0c89155417a3efe0c66da0b58a6
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-29 14:36:31 +0300

    WORKING WITH AOP

--- src/main/java/aop_configuring_around/Vehicle.java
+++ src/main/java/aop_configuring_around/Vehicle.java
@@ -2,6 +2,7 @@ package aop_configuring_around;
 
 /*imports*/
 import bean13_assignment.Speakers;
+import bean13_assignment.Tyres;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,4 +17,43 @@ public class Vehicle {
 
     @Autowired
     private Speakers speakers;
+    private Tyres tyres;
+
+    /*method to play the song*/
+    public String playMusic(boolean vehicleStarted, Song song){
+        return speakers.makeSound(song);
+    }
+
+    /*method to moveVehicle*/
+    public String moveVehicle(boolean vehicleStarted){
+        return tyres.rotate();
+    }
+
+    /*method to stop the vehicle*/
+    public String applyBrake(boolean vehicleStarted){
+        return tyres.stop();
+    }
+
+    /*getter for speakers*/
+    public Speakers getSpeakers(){
+        return speakers;
+    }
+
+    /*setter for speakers*/
+    public void setSpeakers(Speakers speakers){
+        this.speakers = speakers;
+    }
+
+    /*getter for tyres*/
+    public Tyres getTyres(){
+        return  tyres;
+    }
+
+    /*setter for tyres*/
+    public void setTyres(Tyres tyres){
+        this.tyres = tyres;
+    }
+
+
+
 }
