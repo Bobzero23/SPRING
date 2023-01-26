936a9212053ff5e0a857f7461f37c752e36a8b7b
Bobzero <rajabu.mustafa.00@gmail.com>
2023-01-26 15:32:34 +0300

    CUSTOMIZING BEAN NAME

---
+++ Example3_CostumizedBeanName/src/main/java/com/example/main/Main.java
@@ -0,0 +1,25 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        /**A normal Java object */
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda");
+        System.out.println("This bean of the normal Java class is : " + vehicle.getName());
+
+        /**Using Spring Beans*/
+        var context = new AnnotationConfigApplicationContext(ProjConfig.class);
+        Vehicle veh = context.getBean("LAMBO", Vehicle.class);
+        System.out.println("The bean of the IoC Container is: " + veh.getName());
+
+        String hello = context.getBean(String.class);
+        System.out.println("The String bean of the IoC Container is: " + hello);
+
+        Integer number = context.getBean(Integer.class);
+        System.out.println("The Integer bean of the IoC Container is: " + number);
+    }
+}
