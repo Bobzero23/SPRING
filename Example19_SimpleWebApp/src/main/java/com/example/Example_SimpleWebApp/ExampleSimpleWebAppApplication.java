69c4599cdc3485fe5af0af925b1651d0df844d9c
Bobzero <rajabu.mustafa.00@gmail.com>
2023-01-30 13:01:53 +0300

    CREATING A SIMPLE SPRING WEB APP

---
+++ Example19_SimpleWebApp/src/main/java/com/example/Example_SimpleWebApp/ExampleSimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Example_SimpleWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSimpleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSimpleWebAppApplication.class, args);
+	}
+
+}
