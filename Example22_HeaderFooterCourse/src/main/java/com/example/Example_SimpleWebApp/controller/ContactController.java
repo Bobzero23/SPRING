3439b5ae74b27445e3525dee1956af48083a2ee0
Bobzero <rajabu.mustafa.00@gmail.com>
2023-01-30 21:47:01 +0300

    PUTTING CONTACT WEB PAGE FOR EASY SCHOOL APPLICATION

---
+++ Example22_HeaderFooterCourse/src/main/java/com/example/Example_SimpleWebApp/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.example.Example_SimpleWebApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @RequestMapping("/contact")
+    public String displayContactPage(){
+        return "contact.html";
+    }
+}
