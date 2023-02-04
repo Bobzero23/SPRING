626b806873d93644e215c2ba5a0daf7c61d3fe33
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-04 12:56:17 +0300

    REVIEW

--- Example22_HeaderFooterCourse/src/main/java/com/example/Example_SimpleWebApp/controller/ContactController.java
+++ Example22_HeaderFooterCourse/src/main/java/com/example/Example_SimpleWebApp/controller/ContactController.java
@@ -1,13 +1,33 @@
 package com.example.Example_SimpleWebApp.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.logging.Logger;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ContactController {
 
+    private Logger log = (Logger) LoggerFactory.getLogger(ContactController.class);
     @RequestMapping("/contact")
     public String displayContactPage(){
         return "contact.html";
+    }
+
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+                                    @RequestParam String  email, @RequestParam String subject, @RequestParam String message) {
+        log.info("Name : " + name);
+        log.info("Mobile Number : " + mobileNum);
+        log.info("Email Address : " + email);
+        log.info("Subject : " + subject);
+        log.info("Message : " + message);
+        return new ModelAndView("redirect:/contact");
     }
 }
