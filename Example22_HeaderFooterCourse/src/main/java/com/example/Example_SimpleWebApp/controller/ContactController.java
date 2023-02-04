7734f013845f2b844a937204d2cc0bc00c5e6284
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-04 13:42:02 +0300

    GETTING INFORMATION OF THE CLIENT FROM THE UI

--- Example22_HeaderFooterCourse/src/main/java/com/example/Example_SimpleWebApp/controller/ContactController.java
+++ Example22_HeaderFooterCourse/src/main/java/com/example/Example_SimpleWebApp/controller/ContactController.java
@@ -1,20 +1,20 @@
 package com.example.Example_SimpleWebApp.controller;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.logging.Logger;
+
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ContactController {
 
-    private Logger log = (Logger) LoggerFactory.getLogger(ContactController.class);
+    private static Logger log =  LoggerFactory.getLogger(ContactController.class);
     @RequestMapping("/contact")
     public String displayContactPage(){
         return "contact.html";
