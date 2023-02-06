b9fd85bea0471e968f83121366ff17826e9feacb
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-06 21:18:12 +0300

    PERMIT ALL DENY ALL DONE SUCCESSFULLY

--- src/main/java/bean13_assignment/Speakers.java
+++
@@ -1,6 +0,0 @@
-package bean13_assignment;
-
-/*interface for speakers*/
-public interface Speakers {
-    public String makeSound(Song song);
-}
--- src/main/java/bean13_assignment/Tyres.java
+++
@@ -1,7 +0,0 @@
-package bean13_assignment;
-
-/*interface for tyres*/
-public interface Tyres {
-    public  String rotate();
-    public String stop();
-}
