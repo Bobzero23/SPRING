d956720565d03fc9ca56041b96dac1f26e1460a8
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-10 00:19:29 +0300

    SETTING UP THE H2 DATABASE NOT DONE CANT SHOW THE SCHEMA OR THE TABLE

---
+++ Example33_SpringJDBC/src/main/java/com/example/Example_SimpleWebApp/constants/EazySchoolConstants.java
@@ -0,0 +1,7 @@
+package com.example.Example_SimpleWebApp.constants;
+
+public interface EazySchoolConstants {
+    public final String ANONYMOUS = "anonymous";
+    public final String OPEN = "open";
+    public final String CLOSE = "close";
+}
