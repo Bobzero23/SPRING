eac2dbd3df26808328ec729feaffed0aac7d5c5f
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-15 18:34:33 +0300

    DERIVED QUERIES

---
+++ Example37_Auditing/src/main/java/com/example/Example_SimpleWebApp/repository/HolidaysRepository.java
@@ -0,0 +1,15 @@
+package com.example.Example_SimpleWebApp.repository;
+
+import com.example.Example_SimpleWebApp.model.Holiday;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HolidaysRepository extends CrudRepository<Holiday, String> {
+
+}
