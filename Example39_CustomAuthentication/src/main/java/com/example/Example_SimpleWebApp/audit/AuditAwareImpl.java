a469dda7b2c942b7b409e70dc27f0393c84c2adb
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-21 18:27:48 +0300

    CUSTOM AUTHENTICATION

---
+++ Example39_CustomAuthentication/src/main/java/com/example/Example_SimpleWebApp/audit/AuditAwareImpl.java
@@ -0,0 +1,16 @@
+package com.example.Example_SimpleWebApp.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+}
