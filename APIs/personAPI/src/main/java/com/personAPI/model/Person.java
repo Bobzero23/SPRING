0fb0ba70f44df196e0bd5f7b09540a561d700cdd
Bobzero <rajabu.mustafa.00@gmail.com>
2023-03-22 12:32:28 +0300

    model creation completed

--- APIs/personAPI/src/main/java/com/personAPI/model/Person.java
+++ APIs/personAPI/src/main/java/com/personAPI/model/Person.java
@@ -8,24 +8,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     @NotBlank
     private String name;
 
+    @NotBlank
     @Min(0)
     private int age;
-
 }
--- APIs/personAPI/src/main/java/com/personAPI/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.personAPI.repository;
-
-import com.personAPI.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
