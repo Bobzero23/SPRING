efe1d3f37d55cf09ac082a07ff7eb0a321cf96ed
Bobzero <rajabu.mustafa.00@gmail.com>
2023-05-03 21:27:36 +0300

    implementing autowired annotation on setter method

--- Example13_AutowiredOnSetter/src/main/java/com/example/main/Main.java
+++ Example13_AutowiredOnSetter/src/main/java/com/example/main/Main.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjConfig.class);
-
         Person person = context.getBean(Person.class);
-
         System.out.println("The Vehicle bean of the person is: " + person.getVehicle());
     }
 }
